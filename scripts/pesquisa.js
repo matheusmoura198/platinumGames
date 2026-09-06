288ad0705df3f93a606175ed757973bce77ec328
Theus.moura <mateus.leitao6@aluno.ce.gov.br>
2026-09-06 13:27:25 -0300

    Fix typo in hollow knight URL

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -10,7 +10,7 @@ function buscarJogos() {
         "red dead redemption 2": "jogos/reddead2.html",
         "resident evil 5" : "jogos/residentevil5.html",
         "marvel's spiderman" : "jogos/marvel'sspiderman.html",
-        "hollow knight" : "jogos/holowKnight.html",
+        "hollow knight" : "jogos/hollowKnight.html",
         "crash bandicoot 4:it's about time" : "jogos/crashBandicoot4It'sAboutTime.html",
     };
 
